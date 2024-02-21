72abd494be810789994eba7cceb42fecc8b8b4c6
Dinil <dinilgamage5@gmail.com>
2024-02-21 14:14:38 +0530

    todo mongoose model created

---
+++ backend/models/todoModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength:  3,
+    maxlength:  100,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
