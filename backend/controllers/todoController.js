8bdc1c10ec4a2208560c00a4287bc84a57f76caf
Dinil <dinilgamage5@gmail.com>
2024-02-21 16:02:10 +0530

    to do controller functions updated

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,41 +1,94 @@
 const Todo = require('../models/todoModel');
+const mongoose = require('mongoose');
 
+//get all todos
 exports.getTodos = async (req, res) => {
   try {
-    const todos = await Todo.find();
-    res.json(todos);
+    const todos = await Todo.find().sort({ createdAt: -1 });
+    res.status(200).json(todos);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
+//get a single to-do
+exports.getTodo = async (req, res) => {
+  const id = req.params.id;
+
+  //prevent internal errors from invalid id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'Todo not found' });
+  }
+
+  try {
+    const todo = await Todo.findById(id);
+
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    res.status(200).json(todo);
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+//create a new to-do
 exports.createTodo = async (req, res) => {
-  const todo = new Todo({
-    title: req.body.title,
-    completed: req.body.completed,
-  });
+  const title = req.body.title;
 
   try {
-    const newTodo = await todo.save();
-    res.status(201).json(newTodo);
+    const todo = await Todo.create({ title });
+    res.status(200).json(todo);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 };
 
+//update a to-do
 exports.updateTodo = async (req, res) => {
+  const id = req.params.id;
+
+  //prevent internal errors from invalid id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'Todo not found' });
+  }
+
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedTodo);
-  } catch (err) {
+    const todo = await Todo.findOneAndUpdate({_id: id}, {
+      ...req.body
+    })
+
+    if (!todo) {
+        return res.status(404).json({ error: 'Todo not found' });
+      }
+
+    res.status(200).json({ message: 'Todo updated', todo });
+
+  } catch {
     res.status(400).json({ message: err.message });
   }
 };
 
+//delete a to-do
 exports.deleteTodo = async (req, res) => {
+  const id = req.params.id;
+  
+  //prevent internal errors from invalid id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'Todo not found' });
+  }
+    
   try {
-    await Todo.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Todo deleted' });
+    const todo = await Todo.findByIdAndDelete({_id: id});
+
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    res.status(200).json({ message: 'Todo deleted', todo });
+    
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const todoRoutes = require('./routes/todoRoutes')
 //express app
 const app = express()
 
+//middleware
+app.use(express.json());
+
 //routes 
 app.use('/todos', todoRoutes)
 
