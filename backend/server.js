58b79807485e89c7d221a122f1ece51c116513ab
Dinil <dinilgamage5@gmail.com>
2024-02-21 14:13:07 +0530

    mongo db connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
+require ('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+// const todoRoutes = require('./routes/todoRoutes')
 
 //express app
 const app = express()
 
-//listen to requests
-app.listen(process.env.PORT, () => {
-    console.log('Server is listening on port 3000')
-})
+//routes 
+// app.use('/todos', todoRoutes)
+
+//db connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen to requests
+        app.listen(process.env.PORT, () => {
+            console.log('Server is listening on port 3000')
+        })    
+    })
+    .catch((error) => {
+        console.log(error)  
+    })
+
+
