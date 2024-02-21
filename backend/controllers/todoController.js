283eb9035270c1747610afdfd18a4faefd64fa8d
Dinil <dinilgamage5@gmail.com>
2024-02-21 14:35:38 +0530

    minor chnages

---
+++ backend/controllers/todoController.js
@@ -0,0 +1,42 @@
+const Todo = require('../models/todoModel');
+
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createTodo = async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+    completed: req.body.completed,
+  });
+
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,20 @@ require ('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-// const todoRoutes = require('./routes/todoRoutes')
+const todoRoutes = require('./routes/todoRoutes')
 
 //express app
 const app = express()
 
 //routes 
-// app.use('/todos', todoRoutes)
+app.use('/todos', todoRoutes)
 
 //db connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         //listen to requests
         app.listen(process.env.PORT, () => {
-            console.log('Server is listening on port 3000')
+            console.log('Connected to DB and Server is listening on port', process.env.PORT)
         })    
     })
     .catch((error) => {
