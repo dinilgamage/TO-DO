b071ac9ad554705c79302477d47b5753eb3de166
Dinil <dinilgamage5@gmail.com>
2024-02-21 18:23:53 +0530

    Implemented CORS middleware to allow frontend requests

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ require ('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const todoRoutes = require('./routes/todoRoutes')
+const cors = require('cors')
 
 //express app
 const app = express()
 
+//cors config
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    optionsSuccessStatus:  200, 
+  }
+
 //middleware
 app.use(express.json());
+app.use(cors(corsOptions))
 
 //routes 
 app.use('/todos', todoRoutes)
