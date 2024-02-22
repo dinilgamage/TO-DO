35ab25c8f9569239b2848412784895612b01dc92
Dinil <dinilgamage5@gmail.com>
2024-02-22 07:55:19 +0530

    all todos fetched, displayed and styled in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
 //pages and components
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import AddTodo from './components/AddTodo';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
+      <AddTodo />
       <div className="pages">
         <Routes>
           <Route 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+//components
+import TodoItem from '../components/TodoItem';
+
 const Home = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchTodos = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/todos');
+                setTodos(response.data);
+            } catch (error) {
+                console.error('Error fetching todos:', error);
+            }
+        };
+
+        fetchTodos();
+    }, []);
+
     return (
         <div className="home">
-            <h1>Home</h1>
+            {todos && todos.map(todo => (
+                <TodoItem key={todo._id} todo={todo} />
+            ))}
         </div>
     );
 }
