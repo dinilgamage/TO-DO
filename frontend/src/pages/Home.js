d17e2c6355c1c4244fa974bba59c7fde9623d949
Dinil <dinilgamage5@gmail.com>
2024-02-22 20:32:34 +0530

    filter todos and clear completed todos added and styled, home styled for when no todos present

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useTodosContext } from '../hooks/useTodosContext';
+
 
 //components
 import TodoItem from '../components/TodoItem';
 
 const Home = () => {
     const { todos, dispatch } = useTodosContext();
+    const [filter, setFilter] = useState('All');
+
 
     useEffect(() => {
         const fetchTodos = async () => {
@@ -21,13 +24,57 @@ const Home = () => {
         fetchTodos();
     }, []);
 
-    return (
+    const handleClearCompleted = async () => {
+        try {
+          const response = await axios.delete('http://localhost:4000/todos/all/completed');
+          const activeTodos = todos.filter(todo => !todo.completed);
+          dispatch({ type: 'SET_TODOS', payload: activeTodos });
+        } catch (error) {
+          console.error('Error clearing completed tasks:', error);
+        }
+      };
+
+    const filteredTodos = todos ? todos.filter(todo => {
+        switch (filter) {
+          case 'All':
+            return true;
+          case 'Active':
+            return !todo.completed;
+          case 'Completed':
+            return todo.completed;
+          default:
+            return true;
+        }
+      }) : [];
+
+      return (
         <div className="home">
-            {todos && todos.map(todo => (
-                <TodoItem key={todo._id} todo={todo} />
-            ))}
+          {todos && todos.length >  0 ? (
+            <>
+              <div className='tab'>
+                <div className="filter-buttons">
+                  <button className={filter === 'All' ? 'active' : ''} onClick={() => setFilter('All')}>All</button>
+                  <button className={filter === 'Active' ? 'active' : ''} onClick={() => setFilter('Active')}>Active</button>
+                  <button className={filter === 'Completed' ? 'active' : ''} onClick={() => setFilter('Completed')}>Completed</button>
+                </div>
+                <div className='clear-button'>
+                  <button className="clear-completed" onClick={handleClearCompleted}>Clear</button>
+                </div>
+              </div>
+              <div className="todo-list">
+                {filteredTodos.map(todo => (
+                  <TodoItem key={todo._id} todo={todo} />
+                ))}
+              </div>
+            </>
+          ) : (
+            <div className="no-todos-message">
+                <img src="/notodo.svg" alt="Empty list" />
+                <h2>Add your first todo :)</h2>
+            </div>
+          )}
         </div>
-    );
+      );
 }
 
 export default Home;
