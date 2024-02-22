6f0ed5aca53007b6251886087acfe4cc86a9397f
Dinil <dinilgamage5@gmail.com>
2024-02-22 11:27:18 +0530

    add todo implemented with errors shown to user in frontend

--- frontend/src/components/AddTodo.js
+++ frontend/src/components/AddTodo.js
@@ -1,11 +1,35 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 const AddTodo = () => {
   const [title, setTitle] = useState('');
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+        const response = await axios.post('http://localhost:4000/todos', {
+            title,
+        });
+        console.log(response);
+        setTitle('');
+        setError(null);
+        setSuccess('Todo added');
+    } catch (error) {
+        console.error('Error adding todo:', error);
+        setError(error);
+        setSuccess(null);
+    }
+}
+
   
   return (
       <div className="add-todo">
-        <form>
+        {error && <div className="error">{error.message}</div>}
+        {success && <div className="success">{success}</div>}
+        <form onSubmit={handleSubmit}>
         <div className="form-control">
           <input 
             type="text" 
@@ -15,7 +39,8 @@ const AddTodo = () => {
             onChange={(e) => setTitle(e.target.value)}
           />
         </div>
-        <button type="submit">Add</button>
+        <button className="btn-add">Add</button>
+  
       </form>
       </div>  
   );
