fd2438541a0792a9880e705db06a41b683e801af
Dinil <dinilgamage5@gmail.com>
2024-02-23 11:40:31 +0530

    email and password validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -17,6 +18,18 @@ const userSchema = new mongoose.Schema({
 
 //static signup method
 userSchema.statics.signup = async function(email, password) {
+
+    //validation
+    if (!email || !password) {
+        throw new Error('All fields are required');
+    }
+    if (!validator.isEmail(email)) {
+        throw new Error('Invalid email');
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw new Error('Password must be 8+ characters with 1 lower and uppercase, 1 number, and 1 symbol.');
+    }
+
     const exists = await this.findOne({ email });
 
     if (exists) {
